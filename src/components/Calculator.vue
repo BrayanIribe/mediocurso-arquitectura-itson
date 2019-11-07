71ecf0463c97339e188e2960c6780e08873d33c5
Brayan Iribe <brayaniribe@gmail.com>
2019-11-06 17:55:11 -0700

    commit

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,20 +2,9 @@
   <div id='calculator'>
     <b-container>
       <div class='block'>👨‍💻️ Calculadora</div>
-      <div class='row'>
-        <div class='col-sm-4 mb-3 middle'>
-          <b-button
-            v-for='(option, i) in this.options'
-            :key='i'
-            @click='pick(i)'
-            :variant='calcType === i ? "primary" : "secondary"'
-            class='picker'
-            block
-            pill
-          >{{ option.name }}</b-button>
-        </div>
-        <div class='col-sm-4 mb-3'>
-          <b-form @submit.stop.prevent='calculate' novalidate>
+      <b-form @submit.stop.prevent='calculate' novalidate>
+        <div class='row'>
+          <div class='offset-sm-2 col-sm-4 mb-3'>
             <p>{{ helper }}</p>
             <b-form-input
               class='mt-3 mb-3'
@@ -26,24 +15,27 @@
             ></b-form-input>
             <span>Límite de bits</span>
             <b-form-input class='mt-3' type='number' v-model='limit'></b-form-input>
+          </div>
+          <div class='col-sm-4 mb-3'>
+            <template v-if='this.option.type === "dectobin"'>
+              <p class='mb-3'>Resultado Signo Magnitud</p>
+              <b-form-input v-model='result[0]' readonly></b-form-input>
+              <p class='mt-3 mb-3'>Resultado Complemento a dos</p>
+              <b-form-input v-model='result[1]' readonly></b-form-input>
+            </template>
+          </div>
+          <div class='offset-sm-2 col-sm-8'>
             <b-button
               class='mt-3 mb-3'
               variant='outline-light'
+              :disabled='disabled'
               type='submit'
               block
               pill
             >Calcular</b-button>
-          </b-form>
+          </div>
         </div>
-        <div class='col-sm-4 mb-3'>
-          <template v-if='this.option.type === "dectobin"'>
-            <p class='mb-3'>Resultado Signo Magnitud</p>
-            <b-form-input v-model='result[0]' readonly></b-form-input>
-            <p class='mt-3 mb-3'>Resultado Complemento a dos</p>
-            <b-form-input v-model='result[1]' readonly></b-form-input>
-          </template>
-        </div>
-      </div>
+      </b-form>
     </b-container>
   </div>
 </template>
@@ -56,9 +48,10 @@ export default {
       decimal: false,
       number: null,
       result: [],
-      limit: 5,
+      limit: 8,
       options: [],
       calcType: 0,
+      running: false,
       temp: null
     }
   },
@@ -70,6 +63,15 @@ export default {
     },
     option() {
       return this.options[this.calcType]
+    },
+    disabled() {
+      return (
+        this.running ||
+        !this.number ||
+        isNaN(parseFloat(this.number)) ||
+        this.limit > 17 ||
+        Math.abs(this.decimal) > 65534
+      )
     }
   },
   methods: {
@@ -77,65 +79,104 @@ export default {
       this.calcType = i
       this.$refs.in.focus()
     },
+    dectobin(num) {
+      let enteros = parseInt(num)
+      let isNegative = num.indexOf('-', 0) !== -1
+      let decimales = 0
+      if (num.indexOf('.') !== -1) {
+        decimales = num.substr(num.indexOf('.') + 1)
+        decimales = parseInt(decimales)
+      }
+      let result = Math.abs(enteros)
+
+      let overflow_sm = false
+      let overflow_cm = false
+
+      //signo magnitud
+      if (result > Math.pow(2, this.limit - 1) - 1) {
+        overflow_sm = true
+      }
+
+      if (overflow_sm && !isNegative) {
+        overflow_cm = true
+      }
+
+      //complemento a dos
+      let n = Math.abs(enteros) * -1
+      if (
+        (isNegative && enteros === 0) ||
+        n < Math.pow(2, this.limit - 1) * -1
+      ) {
+        overflow_cm = true
+      }
+
+      let code = []
+      let sm = []
+      let cm = []
+      while (true) {
+        code.push(result % 2 ? '1' : '0')
+        result = parseInt(result / 2)
+        if (result === 0) break
+      }
+      code = code.reverse()
+
+      return {
+        enteros,
+        decimales,
+        isNegative,
+        code,
+        overflow_sm,
+        overflow_cm
+      }
+    },
     calculate() {
+      if (this.disabled) return
+      this.running = true
       this.result = []
       if (this.option.type === 'dectobin') {
-        let enteros = parseInt(this.number)
-        let negativo = enteros < 0
-        let decimales = 0
-        if (this.number.indexOf('.') !== -1) {
-          decimales = this.number.substr(this.number.indexOf('.') + 1)
-          decimales = parseInt(decimales)
-        }
-        let result = Math.abs(enteros)
-        let code = ''
-        const t = true
-        while (t) {
-          code += result % 2 ? '1' : '0'
-          result = parseInt(result / 2)
-          if (result === 0) break
-        }
-        code = code.padEnd(this.limit - 1, '0')
-        //signo magnitud
-        let sm = code + (negativo ? '1' : '0')
-        sm = sm
-          .split('')
-          .reverse()
+        let result = this.dectobin(this.number)
+        let sm = result.code
           .join('')
-        if (sm.length <= this.limit) {
-          this.result.push(sm)
-        } else {
-          this.result.push('Overflow')
+          .padStart(this.limit, '0')
+          .split('')
+        let cm = sm
+        if (result.isNegative) {
+          sm[0] = '1'
+          let limit = Math.pow(2, this.limit - 1) * -1
+          cm = Math.abs(limit + Math.abs(this.number))
+          cm = this.dectobin(cm.toString())
+            .code.join('')
+            .padStart(this.limit, '0')
+            .split('')
+          cm[0] = '1'
         }
-        let cm = code.split('')
-        cm = cm.reverse()
-        let cmf = []
-        if (negativo) {
-          let found = false
-          for (let i = cm.length - 1; i >= 0; i--) {
-            if (cm[i] === '1' && !found) {
-              found = true
-              cmf.push(cm[i])
-              continue
-            }
 
-            if (found) cmf.push(cm[i] === '1' ? '0' : '1')
-            else cmf.push(cm[i])
-          }
-          cmf = cmf.reverse()
-        } else {
-          cm.push('0')
-          cmf = cm
-        }
-        console.log('SM => ', sm)
-        console.log('C2 => ', cmf.join(''))
-        if (cmf.length <= this.limit) {
-          this.result.push(cmf.join(''))
-        } else {
-          this.result.push('Overflow')
-        }
-        this.temp = decimales
+        this.result.push(!result.overflow_sm ? sm.join('') : 'Overflow')
+        this.result.push(!result.overflow_cm ? cm.join('') : 'Overflow')
+
+        console.log(
+          '%c ✅ Calculator %c\n',
+          'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
+          'background: transparent; padding:0;',
+          '\nEnteros: ',
+          result.enteros,
+          '\nDecimales: ',
+          result.decimales,
+          '\nEs negativo? ',
+          result.isNegative ? '✅' : '❌',
+          '\nOverflow SM? ',
+          result.overflow_sm ? '✅' : '❌',
+          '\nOverflow CM? ',
+          result.overflow_cm ? '✅' : '❌',
+          '\nDec2Bin => ',
+          result.code,
+          '\nSM => ',
+          sm,
+          '\nCM => ',
+          cm
+        )
       }
+      this.running = false
     }
   },
   created() {
